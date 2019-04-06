852cfefc7851c1f2c2afde17342d0afb06b53238
krzys1ek <combat100@gmail.com>
2019-04-06 19:06:14 +0000

    add color for attacks

--- Monster_Slayer/app.js
+++ Monster_Slayer/app.js
@@ -3,16 +3,23 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
              if (this.checkWin()) {
                  return;
              }
@@ -20,15 +27,25 @@ new Vue({
 
         },
         specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage; 
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
             if (this.checkWin()) {
                 return;
-            }
+            };
             this.monsterAttack();
 
         },
         monsterAttack: function() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Monster for ' + damage
+            });
             this.checkWin();
         },
         heal: function() {
@@ -36,12 +53,16 @@ new Vue({
                 this.playerHealth += 10;
             }else {
                 this.playerHealth = 100;
-            }
+            };
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttack();
 
         },
         giveUp: function() {
-
+            this.gameIsRunning = false;
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
