5877227f68f85edae8b97949f87aa9e70eb88089
krzys1ek <combat100@gmail.com>
2019-04-16 10:45:53 +0000

    fixed class

--- Quotes/src/components/NewQuote.vue
+++ Quotes/src/components/NewQuote.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="row">
         <form>
-            <div class="div col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
                 <label>Quote</label>
                 <textarea class="form-control" rows="3" v-model="quote"></textarea>
             </div>
-            <div class="div col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
                 <div class="button btn btn-primary" @click.prevent="createNew">Add Quote</div>
             </div>
         </form>
