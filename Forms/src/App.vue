aafd561c8ae08ccc7038cc53141fd60b2ed81cc6
krzys1ek <combat100@gmail.com>
2019-04-29 20:22:11 +0000

    commit

--- Forms/src/App.vue
+++ Forms/src/App.vue
@@ -30,7 +30,6 @@
                                 class="form-control"
                                 v-model="userData.age">
                     </div>
-
                 </div>
             </div>
             <div class="row">
@@ -101,13 +100,14 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <app-switch v-model="dataSwitch"></app-switch>
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -148,7 +148,13 @@
                 gender: 'Male',
                 selectedPriority: 'Hight',
                 priorities: ['Hight', 'Medium', 'Low'],
-                dataSwitch: true
+                dataSwitch: true,
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
             }
         },
         components: {
